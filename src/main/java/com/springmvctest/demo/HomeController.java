371932b83c32016784a0d581d7f63b09e4a77875
arhammaroof <arham.maroof@gmail.com>
2019-12-12 18:08:55 +0500

    used custom login/logout pages

--- src/main/java/com/springmvctest/demo/HomeController.java
+++ src/main/java/com/springmvctest/demo/HomeController.java
@@ -1,12 +1,23 @@
 package com.springmvctest.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
     public String home(){
+        //model.addAttribute("msg", new User().getUsername());
         return "index";
+    }
+    @RequestMapping("/login")
+    public String loginPage(){
+        return "login";
+    }
+    @RequestMapping("/logout-success")
+    public String logoutPage()
+    {
+        return "logout";
     }
 }
--- src/main/java/com/springmvctest/demo/SecurityConfig.java
+++ src/main/java/com/springmvctest/demo/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -13,6 +14,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.AntPathMatcher;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +34,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
         return provider;
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests().antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .and()
+                .logout().invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/logout-success").permitAll();
+
+    }
+
+
 //    @Bean
 //    @Override
 //    protected UserDetailsService userDetailsService() {
